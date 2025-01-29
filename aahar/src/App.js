3ff335697617260cda0f419caa28965cba5f10a9
Ankush1105 <personal.ankush001@gmail.com>
2025-01-29 20:02:46 +0530

    Aahar-Frontend : Basic user pannel screeens

---
+++ aahar/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./screens/user_screens/Login";
+import Registration from "./screens/user_screens/Registration";
+import Home from "./screens/user_screens/Home";
+import FoodVendor from "./screens/user_screens/FoodVendor";
+import Contact from "./screens/user_screens/Contact";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/user-home" element={<Home />}></Route>
+        <Route path="/user-home/vendors" element={<FoodVendor />} />
+        <Route path="/user-home/vendors/:id" element={<MessDetails />} />
+
+        <Route path="/user-home/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
