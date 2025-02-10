8054c141081dffb837adbafd1e8f4f29d02cf58a
aditya-s-dhumal <123764620+aditya-s-dhumal@users.noreply.github.com>
2025-02-10 23:07:05 +0530

    Delete aahar-backend/spring_boot_backend_template directory

--- aahar-backend/spring_boot_backend_template/aahar_spring_boot_backend/src/main/java/com/aahar/service/UserSubscriptionService.java
+++
@@ -1,8 +0,0 @@
-package com.aahar.service;
-
-import com.aahar.dtos.UserSubscriptionReqDTO;
-import com.aahar.pojos.UserSubscription;
-
-public interface UserSubscriptionService {
-	public UserSubscription createUserSubscription(UserSubscriptionReqDTO userSubscriptionDto);
-}
